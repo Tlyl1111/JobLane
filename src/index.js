01ea45f7637da5fa250f5d1cf496c0a7dfae7989
Tlyl1111 <yenlinhtranle1111@gmail.com>
2023-11-08 08:50:37 +0700

    Add logo web

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const morgan = require('morgan')
 const handlebars = require('express-handlebars').engine;
+var favicon = require('serve-favicon')
 const path = require('path');
 
 const app = express();
 const port = 3101;
+
+app.use(favicon(path.join(__dirname, 'public', 'img', 'logoJL.png')))
 
 app.use(morgan('combined'))
 
